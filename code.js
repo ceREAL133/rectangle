4f4f30cbde59f653e4b91340a5be6e68cc9ce37d
ceREAL133 <72147047+ceREAL133@users.noreply.github.com>
2021-02-27 16:13:28 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
+let showKeyTrue = document.getElementById('showKeyTrue')
+
 function keyMaker() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234key6789%^&";
-  
+
     for (var i = 0; i < 1; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
   
@@ -14,18 +16,18 @@ let arr = [];
     min = 0;
     max = 99;
 var possibleSym = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234key6789%^&";    
-    
-    for (let i = 0; i < 11; i++) {  //vertical quantity
-        arr[i] = [];
-        for (let j = 0; j < 11; j++) {  //horizontal quantity
-            arr[i][j] = possibleSym.charAt(Math.floor(Math.random() * possibleSym.length));
-        }
+  
+for (let i = 0; i < 11; i++) {  //vertical quantity
+    arr[i] = [];
+    for (let j = 0; j < 11; j++) {  //horizontal quantity
+        arr[i][j] = possibleSym.charAt(Math.floor(Math.random() * possibleSym.length));
     }
+}
 
-    for (let i = 0; i < 11; i++) {
-        for (let j = 0; j < 11; j++) {
-            console.log(arr[i][j] + " ");
-        }
+for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < 11; j++) {
+        console.log(arr[i][j] + " ");
+    }
         arr[2][9] = key;
         arr[3][8] = key;
         arr[4][7] = key;
@@ -35,31 +37,23 @@ var possibleSym = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234key6
         arr[8][3] = key;
         arr[9][2] = key;
         arr[10][1] = key;
-    }
+}
 
-    // mesh generator
-    let counter = 99;
-    document.write ('<table width="100%" border="1">');
-    for (i=1; i<=11; i++) {
-     
-      document.writeln("<tr>");
-      for (j=1; j<11; j++){ ;
-          document.write("<td>" + `${counter} __ ${arr[i][j]}` + "<\/td>")
-            counter--;
-        }
+// mesh generator
+let counter = 99;
+document.write ('<table width="100%" border="1">');
+for (i=1; i<=11; i++) {
+    document.writeln("<tr>");
+    
+    for (j=1; j<11; j++){ ;
+        document.write("<td>" + `${counter} __ ${arr[i][j]}` + "<\/td>")
+        counter--;
+    }
       document.writeln("<\/tr>");
-    }
-    document.write ("<\/table> ");
+}
 
+document.write ("<\/table> ");
 
-    //this part of code doesn`t work
-    function saveKeyToSessionStorage() {
-        window.sessionStorage.setItem('value', JSON.stringify(key))
-    }
-
-    window.onload = saveKeyToSessionStorage();
-
-    
-
-    
-    
+function showKey() {
+    showKeyTrue.textContent = `this is ur symbol- ${key}`
+}
